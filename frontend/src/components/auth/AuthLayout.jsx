160d5633bb75743f78add8edaa1e52e744cb92c4
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2025-03-25 00:26:45 +0530

    changed the name

--- frontend/src/components/auth/AuthLayout.jsx
+++ frontend/src/components/auth/AuthLayout.jsx
@@ -5,7 +5,7 @@ function AuthLayout() {
     <div className="flex min-h-screen">
 
       <div className="hidden md:flex items-center justify-center bg-black w-1/3 p-8">
-        <h1 className="text-5xl font-bold text-white">EdAll</h1>
+        <h1 className="text-5xl font-bold text-white">PathWay</h1>
       </div>
 
       <div className="flex flex-1 items-center justify-center bg-gray-100 p-8 w-full">
